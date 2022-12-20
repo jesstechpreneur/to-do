c9a14bfd50747f3f9159ece325e5b3f020ebbbfa
Jess <hi@jesstechpreneur.com>
2022-12-20 06:31:39 +0000

    todo functions

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,23 +1,40 @@
-import React from "react";
-import styled from "styled-components";
+import React, {useState, useEffect} from "react";
+import Form from "../component/form.jsx";
+import TodoList from "./ToDoList.jsx";
 
 //create your first component
 const Home = () => {
+
+	const[inputText, setInputText] = useState("");
+	const [todos, setTodos] = useState([]);
+	const [status, setStatus] = useState("all");
+	const [filteredTodos, setFilteredTodos] = useState([]);
+
+	const filterHandler = () => {
+		switch(status){
+			case 'completed':
+				setFilteredTodos(todos.filter((todo) => todo.completed === true))
+				break;
+			case 'uncompleted':
+				setFilteredTodos(todos.filter((todo) => todo.completed === false))
+				break;
+			default:
+				setFilteredTodos(todos);
+				break;
+		}
+	};
+
+	useEffect(() => {
+		filterHandler();
+	}, [todos, status]);
+
 	return (
 		<div className="text-center todo">
-			<h1 className="text-center mt-5">To-Do List</h1>
-			<input />
-			<a href="#" className="btn btn-success">
-				Submit
-			</a>
-			<ul>
-				<li>Task 1 <span class="delete">delete</span></li>
-				<li>Task 2 <span class="delete">delete</span></li>
-				<li>Task 3 <span class="delete">delete</span></li>
-				<li>Task 4 <span class="delete">delete</span></li>
-			</ul>
-			<p>Tasks Created: #</p>
-			<p>Tasks left: #</p>
+			<header>
+				<h1 className="text-center mt-5">To-Do List</h1>
+			</header>
+			<Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} setStatus={setStatus}  />
+			<TodoList  setTodos={setTodos} todos={todos} filteredTodos={filteredTodos}/>
 			
 		</div>
 	);
